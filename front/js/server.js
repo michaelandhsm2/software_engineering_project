010d70f82a9684b95b00bbab49912db1c067fbff
Michael Fu <michaelandhsm2@gmail.com>
2018-01-02 10:18:32 +0800

    Header Logout Function Added

--- front/js/server.js
+++ front/js/server.js
@@ -1,2 +1,7 @@
 // var SERVER_API = "http://140.124.183.30:8080/SE_Final/api";
 var SERVER_API = "http://127.0.0.1:8080/SE_final/api";
+
+function logout(){
+  window.sessionStorage.clear();
+  location.href= ("index.html");  
+}
